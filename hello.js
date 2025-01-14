6c079f9221c1576bc74bc3221e51c6dbc207fe76
Hari <114710870+ThReT0@users.noreply.github.com>
2025-01-14 20:44:31 +0000

    bart

---
+++ hello.js
@@ -0,0 +1,8 @@
+let button = document.getElementById("button");
+        button.addEventListener('click', () =>{
+            let heading = document.getElementById("headingID");
+            //heading.innerText = "Hala MADRID";
+            let a = prompt("If you like real madrid fan enter your Name");
+            alert("Do not be a glory Hunter");
+            heading.innerText = a;
+        });
